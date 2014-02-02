deaae2dd22603c0463c58fcae977b17e9b71c235
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-02 12:56:51 +0000

    my_inject method variation

--- lib/ArrayInject.rb
+++ lib/ArrayInject.rb
@@ -1,13 +1,18 @@
 class Array
 
-def my_inject(acc=0)
-    self.each { |i| acc = yield(acc, i)}
-    acc  
+# def my_inject(acc=0)
+#     self.each { |i| acc = yield(acc, i)}
+#     acc  
+#   end
+# end 
+
+def my_inject(accumulator)
+  self.each do |item|
+    accumulator = yield accumulator, item
   end
-end 
-
-
-
+  accumulator 
+end
+end
 
 
 
